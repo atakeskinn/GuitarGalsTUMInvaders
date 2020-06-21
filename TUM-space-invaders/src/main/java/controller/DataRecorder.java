36a4b622b643e4e2c271f777893f5948ebb3b205
Pydes-boop <59734957+Pydes-boop@users.noreply.github.com>
2020-06-21 16:37:53 +0200

    Added basic functionality to the Data and DataRecorder class

--- TUM-space-invaders/src/main/java/controller/DataRecorder.java
+++ TUM-space-invaders/src/main/java/controller/DataRecorder.java
@@ -1,12 +1,20 @@
 package controller;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class DataRecorder {
 	private List<Data> recordedData;
-	
+
+	public DataRecorder() {
+		recordedData = new ArrayList<Data>();
+	}
+
 	public void recordData(Data data) {
 		recordedData.add(data);
+		//You use this by adding:
+		//"DataRecorder.recordData(Data.recordData(String Info));"
+		//to your Moment/Event in Code that you want to record
 	}
 
 	public void saveData() {
