82aabb0474fe104bbb0bda9b499534fdf56de067
ge75sur <ata.keskin@tum.de>
2020-06-23 13:44:25 +0200

    wave object implemented (a bit)

--- TUM-space-invaders/src/main/java/controller/SoundManager.java
+++ TUM-space-invaders/src/main/java/controller/SoundManager.java
@@ -1,10 +1,17 @@
 package controller;
 
-public class SoundManager {
-	public void playBackgroundMusic() {}
-	public void playPlayerShootSound() {}
-	public void playEnemyShootSound() {}
-	public void playPlayerDeathSound() {}
-	public void playEnemyDeathSound(){}
-    public void stopBackgroundMusic() {}
+public interface SoundManager {
+
+	public static final String BACKGROUND_MUSIC_FILE = "audio/.mp3";
+	public static final String PLAYER_SHOOT_SOUND_FILE = "audio/.mp3";
+	public static final String ENEMY_SHOOT_SOUND_FILE = "audio/.wav";
+	public static final String PLAYER_DEATH_SOUND_FILE = "audio/.wav";
+
+	public void playBackgroundMusic();
+	public void playPlayerShootSound();
+	public void playEnemyShootSound();
+	public void playPlayerDeathSound();
+	public void playEnemyDeathSound();
+	public void playWaveEndSound();
+    public void stopBackgroundMusic();
 }
--- TUM-space-invaders/src/main/java/view/GameUI.java
+++ TUM-space-invaders/src/main/java/view/GameUI.java
@@ -108,7 +108,8 @@ public class GameUI extends Canvas implements Runnable {
 	 */
 	public void gameSetup() {
 		this.gameInternal = new GameInternal(this.size);
-		this.gameInternal.setSoundManager(new SoundManager());
+		//TODO: implement sound manager
+		this.gameInternal.setSoundManager(new ImplementedSoundManager());
 		this.widthProperty().set(this.size.getWidth());
 		this.heightProperty().set(this.size.getHeight());
 
