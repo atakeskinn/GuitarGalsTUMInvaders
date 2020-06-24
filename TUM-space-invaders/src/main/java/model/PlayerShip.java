84bd446be39ef4633baf4fda673bc98afdbbc445
BjornKuehne <BjornKuehne@users.noreply.github.com>
2020-06-24 15:23:39 +0100

    Revert "Merge branch 'master' of https://github.com/atakeskinn/GuitarGalsTUMInvaders"
    This reverts commit 1c3d1283b81865387716125550eb0d285e936a99, reversing
    changes made to b1a6f0497d08178a69e6c16110c72784307e8cac.

--- TUM-space-invaders/src/main/java/model/PlayerShip.java
+++ TUM-space-invaders/src/main/java/model/PlayerShip.java
@@ -1,19 +1,10 @@
 package model;
 
-import javafx.geometry.Dimension2D;
-
-import javafx.geometry.Point2D;
+import java.awt.geom.Point2D;
 
 public class PlayerShip {
-	public static Dimension2D DEFAULT_SIZE = new Dimension2D(30 /2 *3, 20 /2 * 3);
-	public static String IMAGE_NAME = "player";
-
-	private Dimension2D screenSize;
-
-	private Dimension2D dimension;
+	//TODO: import all attributes
 	private Point2D pos;
-	private double speedX;
-	private boolean alive;
 
 	public Point2D getPos() {
 		return pos;
@@ -23,45 +14,8 @@ public class PlayerShip {
 		this.pos = pos;
 	}
 
-	public PlayerShip(Dimension2D screenSize) {
-		this.screenSize = screenSize;
-		this.dimension = DEFAULT_SIZE;
-		initPlayer((int) ((screenSize.getWidth()/2) - (dimension.getWidth()/2)), 500);
+	public void shoot() {
+		//TODO shoot
 	}
-	private void initPlayer(int x, int y) {
-		pos = new Point2D(x, y);
-		this.speedX = 0;
-		alive = true;
-	}
-
-	public void update() {
-		if(!alive) return;
-		pos = pos.add(speedX, 0);
-		if (pos.getX() <= 2) {
-			pos = new Point2D(2, pos.getY());
-		}
-		if (pos.getX() + 2 >= screenSize.getWidth() - dimension.getWidth()) {
-			pos = new Point2D(screenSize.getWidth() - dimension.getWidth() - 2, pos.getY());
-		}
-	}
-
-	public void setSpeedX(double speedX) {
-		this.speedX = speedX;
-	}
-
-	public Dimension2D getDimension() {
-		return dimension;
-	}
-
-	public boolean isAlive() {
-		return alive;
-	}
-
-	public void setAlive(boolean alive) {
-		this.alive = alive;
-	}
-
-	public static String getImage() {
-		return IMAGE_NAME;
-	}
+	
 }
--- TUM-space-invaders/src/main/java/model/Wave.java
+++ TUM-space-invaders/src/main/java/model/Wave.java
@@ -114,12 +114,54 @@ public class Wave {
         return index / width;
     }
 
+    private int getRightMostEnemyIndex() {
+
+        int rightMost = -1;
+		for (int i = 0; i < listOfEnemies.size(); i++) {
+			EnemyShip current = listOfEnemies.get(i);
+
+			if(current.isAlive()) {
+				if(rightMost < 0) {
+					rightMost = i;
+				} else if(getX(i) > getX(rightMost)) {
+					rightMost = i;
+				}
+			}
+		}
+		return rightMost;
+    }
+
     private Optional<EnemyShip> getRightMostEnemy() {
-    	return listOfEnemies.parallelStream().filter(EnemyShip::isAlive).max(Comparator.comparing(enemyShip -> enemyShip.getPos().getX()));
+    	int i = getRightMostEnemyIndex();
+    	if(i < 0)
+    		return Optional.empty();
+    	else
+    		return Optional.of(listOfEnemies.get(i));
+	}
+
+	private int getLeftMostEnemyIndex() {
+
+		int leftMost = -1;
+		for (int i = 0; i < listOfEnemies.size(); i++) {
+			EnemyShip current = listOfEnemies.get(i);
+
+			if(current.isAlive()) {
+				if(leftMost < 0) {
+					leftMost = i;
+				} else if(getX(i) < getX(leftMost)) {
+					leftMost = i;
+				}
+			}
+		}
+		return leftMost;
 	}
 
 	private Optional<EnemyShip> getLeftMostEnemy() {
-		return listOfEnemies.parallelStream().filter(EnemyShip::isAlive).min(Comparator.comparing(enemyShip -> enemyShip.getPos().getX()));
+		int i = getLeftMostEnemyIndex();
+		if(i < 0)
+			return Optional.empty();
+		else
+			return Optional.of(listOfEnemies.get(i));
 	}
 
 	public void update() {
