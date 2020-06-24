a66b14eda260d46dff7c9b373691474a6efd6d93
BjornKuehne <BjornKuehne@users.noreply.github.com>
2020-06-24 15:24:37 +0100

    Revert "Revert "Merge branch 'master' of https://github.com/atakeskinn/GuitarGalsTUMInvaders""
    This reverts commit dc7e4936ad1946a7ba27bc8032c07d792015a214.

--- TUM-space-invaders/src/main/java/controller/PlayerController.java
+++ TUM-space-invaders/src/main/java/controller/PlayerController.java
@@ -9,21 +9,33 @@ import model.PlayerShip;
 import view.GameUI;
 
 public class PlayerController {
-	private EventHandler<KeyEvent> keyHandler;
-	private GameUI gameUI;
-	private PlayerShip player;
+    private EventHandler<KeyEvent> keyHandler;
+    private GameUI gameUI;
+    private PlayerShip player;
 
-	public PlayerController(GameUI gameUI, PlayerShip player) {
-	    this.player = player;
+    public PlayerController(GameUI gameUI, PlayerShip player) {
+        this.player = player;
 
         keyHandler = e -> {
-            //DO IT LIKE THIS ↓↓
-            //if(e.getCode() == KeyCode.A) do stuff {};
-            //TODO: Key Press Handling
+            if(!player.isAlive()) return;
+            KeyCode key = e.getCode();
+            if (key == KeyCode.D) {
+                player.setSpeedX(2);
+            }
+            else if (key == KeyCode.A) {
+                player.setSpeedX(-2);
+            }
+            if (key == KeyCode.SPACE) {
+                gameUI.getGameInternal().createProjectile(player.getPos().add(player.getDimension().getWidth() / 2, 0), -5);
+            }
         };
 
         this.gameUI = gameUI;
         this.gameUI.addEventHandler(KeyEvent.KEY_PRESSED, keyHandler);
     }
-	
+
+    public void remove() {
+        this.gameUI.removeEventHandler(KeyEvent.KEY_PRESSED, keyHandler);
+    }
+
 }
